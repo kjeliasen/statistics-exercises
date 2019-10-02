56a26f748d9b8b3eef1b9624f26684093bb8cd2d
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-10-02 10:51:27 -0500

    zach's notes

---
+++ simulations_zach.py
@@ -0,0 +1,54 @@
+# Two Dice
+rolls = np.random.randint(1, 7, (10000, 2))
+p_same_values = (rolls[:,0] == rolls[:, 1]).mean()
+
+
+flips np.random.choice(['Heads','Tails'] (10000, 8))
+p_3heads = ((flips == 'Heads').sum(axis=1) == 3))
+p_gt3heads = ((flips == 'Heads').sum(axis=1) > 3))
+
+
+#2 students on billboards
+options = ['Web Dev'] * 3 + ['Data Science']
+r_2billboards = (np.random.choice(options, (10000, 2)) == 'Data Science').sum(axis=1) == 2
+p_2billboards = r_2billboards.mean()
+
+sams = (np.random.choice(options, (10000, 2)) == 'Data Science').prod(axis=1).mean()
+
+# Poptarts
+
+pt_consumption = np.round(np.random.normal(3, 1.5, (10000, 5)))
+pt_consumption = np.where(pt_consumption < 0, 0, pt_consumption)
+p_poptart = (pt_consumption.sum(axis=1) < 17).mean()
+
+#compare heights
+
+male_heights = np.random.normal(178, 8, 10000)
+female_heights = np.random.normal(170, 6, 10000)
+p_taller_female = (female_heights > male_heights).mean()
+
+#anaconda installation
+
+p_corruption = 1/250
+
+p_corrupt = lambda x: ((np.random.random((10000, x)) < p_corruption).sum(axis=1) > 0).mean()
+p_uncorrupt = lambda x: 1 - ((np.random.random((10000, x)) < p_corruption).sum(axis=1) > 0).mean()
+
+jackies_array = np.random.choice(['Success', 'Corrupted'], (10000, 450), p=[249/250, 1/250])
+p_jackies = ((jackies_array == 'Corrupted').sum(axis=1) == 0).mean()
+
+
+# Food Truck
+
+p_truck = .7
+trucks = lambda x: ((np.random.choice(['Truck', 'No Truck'], (10000,x), p=(p_truck, 1-p_truck)) == 'No Truck').all(axis=1)).mean()
+
+
+# Birthdays
+
+birthdays = lambda x: np.random.choice(range(365), (10000, x))
+unique_bdays = lambda x: np.array([np.unique(row).size for row in birthdays(x)])
+p_unique_bdays = lambda x: (unique_bdays(x) == x).mean()
+
+
+p_from_pandas = lambda x: (pd.DataFrame(birthdays(x)).nunique(axis=1) < x).mean()
