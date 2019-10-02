88c460449b8bddd8d0d87e4a414406643bb55918
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-10-02 09:01:39 -0500

    Finished lesson (so I think)

--- simulation.py
+++ simulation.py
@@ -10,7 +10,6 @@ import datetime as dt
 
 from colorama import init, Fore, Back, Style
 
-np.random.seed(123)
 
 txt=''
 title_fancy = Style.BRIGHT + Fore.YELLOW + Back.BLUE
@@ -72,6 +71,29 @@ def get_db_url(user, password, host, database):
     return f'mysql+pymysql://{user}:{password}@{host}/{database}'
 
 
+#    return np.random.choice(np.arange(2), (n_times, n_coins))
+
+
+#print_title('Title_goes_here')
+#print_rule('This\nis\nrule\ntext')
+
+clear_screen()
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+print_title('Exercises')
+
+print_rule('''Create a file named simulation.py for this exercise.'''
+, intro_fancy)
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+np.random.seed(123)
+n_trials = 100000
 
 
 def roll_dice(n_dice=2, n_trials=10, n_sides=6, base=1): 
@@ -95,29 +117,12 @@ def select_normal(mean, std, n_sets=1, n_trials=1000):
 
 def select_normal_int(mean, std, n_sets=1, n_trials=1000): 
     non_int_results = select_normal(mean=mean, std=std, n_sets=1, n_trials=n_trials * n_sets)
-    int_results = np.array([max(0,int(result)) for result in non_int_results])
+    int_results = np.array([max(0,round(float(result))) for result in non_int_results])
     return int_results.reshape(n_trials, n_sets)
 
 
 def flip_coins(n_coins, n_times, base=0):
     return (roll_dice(n_dice=n_coins, n_trials=n_trials, n_sides=2, base=base))
-
-#    return np.random.choice(np.arange(2), (n_times, n_coins))
-
-
-#print_title('Title_goes_here')
-#print_rule('This\nis\nrule\ntext')
-
-clear_screen()
-
-###############################################################################
-###############################################################################
-###############################################################################
-
-print_title('Exercises')
-
-print_rule('''Create a file named simulation.py for this exercise.'''
-, intro_fancy)
 
 
 ###############################################################################
@@ -128,13 +133,14 @@ print_title('Problem 1')
 
 print_rule('''How likely is it that you roll doubles when rolling two dice?''')
 
-n_trials = 100000
-n_dice = 2
-rolls = roll_dice(n_dice, n_trials)
-is_pair = sum([1 for roll in rolls if len(set(roll)) == 1])
-p_equal = is_pair / n_trials
 
-print(f'{title_fancy}Sample hit {is_pair} our of {n_trials} times, for a probability of {100 * p_equal:.2f}%{Style.RESET_ALL}')
+def roll_matches(n_dice=2, n_trials=n_trials):
+    rolls = roll_dice(n_dice, n_trials)
+    is_pair = sum([1 for roll in rolls if len(set(roll)) == 1])
+    p_equal = is_pair / n_trials
+    print(f'{demo_fancy}Sample hit {is_pair} out of {n_trials} times, for a probability of {100 * p_equal:.2f}%{Style.RESET_ALL}')
+
+p_all_equal = roll_matches(n_dice=2)
 
 ###############################################################################
 ###############################################################################
@@ -145,13 +151,16 @@ print_title('Problem 2')
 print_rule('''If you flip 8 coins, what is the probability of getting exactly 3 heads? What 
 is the probability of getting more than 3 heads?''')
 
-n_trials = 100000
-n_coins = 8
-flips = flip_coins(n_coins, n_trials)
-three_heads = sum([1 for flip in flips if sum(flip) == 3])
-p_3_heads = three_heads / n_trials
 
-print(f'{title_fancy}Sample hit {three_heads} our of {n_trials} times, for a probability of {100 * p_3_heads:.2f}%{Style.RESET_ALL}')
+def flip_heads(n_coins=8, n_heads=3, n_trials=n_trials):
+    flips = flip_coins(n_coins, n_trials)
+    three_heads = sum([1 for flip in flips if sum(flip) == 3])
+    p_n_heads = three_heads / n_trials
+    print(f'{demo_fancy}Sample hit {three_heads} out of {n_trials} times, for a probability of {100 * p_n_heads:.2f}%{Style.RESET_ALL}')
+    return p_n_heads
+
+
+p_heads = flip_heads(n_heads=3)
 
 ###############################################################################
 ###############################################################################
@@ -167,11 +176,15 @@ data science students on them?''')
 n_trials = 100000
 n_cohorts = 4
 n_boards = 2
-sightings = roll_dice(n_dice=n_boards, n_trials=n_trials, n_sides=n_cohorts)
-two_datas = sum([1 for sighting in sightings if len(set(sighting)) == 1 and sighting[0] == 1])
-p_2_datas = two_datas / n_trials
 
-print(f'{title_fancy}Sample hit {two_datas} our of {n_trials} times, for a probability of {100 * p_2_datas:.2f}%{Style.RESET_ALL}')
+def data_boards(n_boards=n_boards, n_cohorts=n_cohorts, n_trials=n_trials):
+    sightings = roll_dice(n_dice=n_boards, n_trials=n_trials, n_sides=n_cohorts)
+    two_datas = sum([1 for sighting in sightings if len(set(sighting)) == 1 and sighting[0] == 1])
+    p_2_datas = two_datas / n_trials
+    print(f'{demo_fancy}Sample hit {two_datas} out of {n_trials} times, for a probability of {100 * p_2_datas:.2f}%{Style.RESET_ALL}')
+    return p_2_datas
+
+p_datas = data_boards()
 
 ###############################################################################
 ###############################################################################
@@ -187,11 +200,16 @@ afternoon?''')
 n_trials = 100000
 mean = 3
 std = 1.5
-tarts_popped = select_normal_int(mean=mean, std=std, n_sets=5, n_trials=n_trials)
-unpopped_tarts = sum([1 for pop in tarts_popped if sum(pop) <= 16])
-p_tarts = unpopped_tarts / n_trials
+stock = 17
 
-print(f'{title_fancy}Sample hit {unpopped_tarts} our of {n_trials} times, for a probability of {100 * p_tarts:.2f}%{Style.RESET_ALL}')
+def tarts_popped(mean=mean, std=std, stock=stock, n_trials=n_trials):
+    tarts_popped = select_normal_int(mean=mean, std=std, n_sets=5, n_trials=n_trials)
+    unpopped_tarts = sum([1 for pop in tarts_popped if sum(pop) < stock])
+    p_tarts = unpopped_tarts / n_trials
+    print(f'{demo_fancy}Sample hit {unpopped_tarts} out of {n_trials} times, for a probability of {100 * p_tarts:.2f}%{Style.RESET_ALL}')
+    return p_tarts
+
+p_tarts = tarts_popped(stock=17)
 
 ###############################################################################
 ###############################################################################
@@ -208,14 +226,17 @@ mean_men = 178
 std_men = 8
 mean_women = 170
 std_women = 6
-heights_men = select_normal(mean=mean_men, std=std_men, n_sets=1, n_trials=n_trials)
-heights_women = select_normal(mean=mean_women, std=std_women, n_sets=1, n_trials=n_trials)
-heights_all = list(zip(heights_men, heights_women))
-taller_women = sum([1 for heights in heights_all if heights[0] < heights[1]])
-p_taller_woman = taller_women / n_trials
 
-print(f'{title_fancy}Sample hit {taller_women} our of {n_trials} times, for a probability of {100 * p_taller_woman:.2f}%{Style.RESET_ALL}')
+def compare_heights(n_trials=n_trials, mean_men=mean_men, mean_women=mean_women, std_men=std_men, std_women=std_women):
+    heights_men = select_normal(mean=mean_men, std=std_men, n_sets=1, n_trials=n_trials)
+    heights_women = select_normal(mean=mean_women, std=std_women, n_sets=1, n_trials=n_trials)
+    heights_all = list(zip(heights_men, heights_women))
+    taller_women = sum([1 for heights in heights_all if heights[0] < heights[1]])
+    p_taller_woman = taller_women / n_trials
+    print(f'{demo_fancy}Sample hit {taller_women} out of {n_trials} times, for a probability of {100 * p_taller_woman:.2f}%{Style.RESET_ALL}')
+    return p_taller_woman
 
+p_taller_woman = compare_heights()
 
 ###############################################################################
 ###############################################################################
@@ -228,35 +249,42 @@ that the download is corrupted and the installation fails. What are the odds
 that after having 50 students download anaconda, no one has an installation 
 issue?''') 
 
-n_trials = 100000
+###############################################################################
+
 n_risk = 250
 
-def get_good_installs(n_users, n_risk, n_trials):
+
+def get_good_installs(n_users=50, n_risk=250, n_trials=n_trials):
     install_results = roll_dice(n_dice=n_users, n_trials=n_trials, n_sides=n_risk)
     good_installs = sum([1 for result in install_results if 1 not in result])
     p_all_good = good_installs / n_trials
-    print(f'{title_fancy}Sample hit {good_installs} out of {n_trials} times with {n_users} users, for a probability of {100 * p_all_good:.2f}%{Style.RESET_ALL}')
-    return install_results, good_installs, p_all_good
+    print(f'{demo_fancy}Sample hit {good_installs} out of {n_trials} times with {n_users} users, for a probability of {100 * p_all_good:.2f}%{Style.RESET_ALL}')
+    return p_all_good
 
-install_results, good_installs, p_all_good = get_good_installs(n_users=50, n_risk=n_risk, n_trials=n_trials)
+###############################################################################
 
+p_all_good = get_good_installs(n_users=50, n_risk=n_risk, n_trials=n_trials)
+
+###############################################################################
 
 print_rule('''100 students?''')
 
-install_results, good_installs, p_all_good = get_good_installs(n_users=100, n_risk=n_risk, n_trials=n_trials)
+p_all_good = get_good_installs(n_users=100, n_risk=n_risk, n_trials=n_trials)
 
+###############################################################################
 
 print_rule('''What is the probability that we observe an installation issue within the first 
 150 students that download anaconda?''')
 
-install_results, good_installs, p_all_good = get_good_installs(n_users=150, n_risk=n_risk, n_trials=n_trials)
-print(f'\n{title_fancy}There is a probability of {100 * (1 - p_all_good):.2f}% chance of encountering an error{Style.RESET_ALL}')
+p_all_good = get_good_installs(n_users=150, n_risk=n_risk, n_trials=n_trials)
 
+print(f'\n{demo_fancy}There is a probability of {100 * (1 - p_all_good):.2f}% chance of encountering an error{Style.RESET_ALL}')
+
+###############################################################################
 
 print_rule('''How likely is it that 450 students all download anaconda without an issue?''')
 
-install_results, good_installs, p_all_good = get_good_installs(n_users=450, n_risk=n_risk, n_trials=n_trials)
-
+p_all_good = get_good_installs(n_users=450, n_risk=n_risk, n_trials=n_trials)
 
 ###############################################################################
 ###############################################################################
@@ -268,11 +296,23 @@ print_rule('''There's a 70% chance on any given day that there will be at least 
 truck at Travis Park. However, you haven't seen a food truck there in 3 days. 
 How unlikely is this?''')
 
+def truck_appears(n_days=7, chance=70, chances=100, n_trials=n_trials):
+    days_charted = roll_dice(n_dice=n_days, n_trials=n_trials, n_sides=chances)
+    appeared = sum([1 for day_value in days_charted if min(day_value) < chance])
+    p_appears = appeared / n_trials
+    print(f'{demo_fancy}For {n_days} days, sample truck arrived {appeared} out of {n_trials} times, for a probability of {100 * p_appears:.2f}%{Style.RESET_ALL}')
+    return p_appears
+
+p_appears = truck_appears(n_days=3)
+
+print(f'\n{demo_fancy}There is a {100 * (1 - p_appears):.2f}% chance no truck will appear in 3 days{Style.RESET_ALL}')
+
+###############################################################################
 
 
 print_rule('''How likely is it that a food truck will show up sometime this week?''')
 
-
+p_appears = truck_appears(n_days=7)
 
 ###############################################################################
 ###############################################################################
@@ -283,9 +323,26 @@ print_title('Problem 8')
 print_rule('''If 23 people are in the same room, what are the odds that two of them share a 
 birthday? ''')
 
+def match_birthdays(n_people=23, n_trials=n_trials, n_days=365):
+    rolls = roll_dice(n_dice=n_people, n_trials=n_trials, n_sides=n_days)
+    has_matches = sum([1 for roll in rolls if len(set(roll)) != n_people])
+    p_has_match = has_matches / n_trials
+    print(f'{demo_fancy}With {n_people} people, sample hit {has_matches} out of {n_trials} times, for a probability of {100 * p_has_match:.2f}%{Style.RESET_ALL}')
+    return p_has_match
+
+
+###############################################################################
+
+p_has_match = match_birthdays(n_people=23)
+
+###############################################################################
 
 print_rule('''What if it's 20 people? ''')
 
+p_has_match = match_birthdays(n_people=20)
+
+###############################################################################
 
 print_rule('''40?''')
 
+p_has_match = match_birthdays(n_people=40)
